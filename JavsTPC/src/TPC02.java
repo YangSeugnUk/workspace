da72729b3aad43cbc2999c20a12777dff18ddf93
SeungUk <9488372@hanmail.net>
2022-10-14 21:38:11 +0900

    프로그래밍 3대요소

---
+++ JavsTPC/src/TPC02.java
@@ -0,0 +1,26 @@
+
+public class TPC02 {
+
+	public static void main(String[] args) {
+		// 프로그래밍의 3대요소 : 변수, 자료형(DataType), 할당(=)
+		// 1+1 = 2 -> 메모리에서 계산된다. 
+		int a,b,c;
+		a=1;
+		b=1;
+		c=a+b; // 2
+		System.out.println(c);
+		
+		float f; //float, double
+		f=34.5f;
+		
+		char d;
+		d = 'A';
+		
+		boolean g;
+		g=true;
+		
+		
+
+	}
+
+}
