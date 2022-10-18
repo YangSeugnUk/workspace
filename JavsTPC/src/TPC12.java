8338af360fe05961db32e1890d33ef02a32d8c11
SeungUk <9488372@hanmail.net>
2022-10-18 18:31:51 +0900

    객체가 메모리에 어떻게 만들어지나
    디폴트생성자, 중복생성자(overloading)

---
+++ JavsTPC/src/TPC12.java
@@ -0,0 +1,18 @@
+import kr.tpc.BookVO;
+
+public class TPC12 {
+	public static void main(String[] args) {
+		// 생성자 -> 생성+초기화 -> 중복정의 가능
+		 BookVO b1 = new BookVO();
+		 System.out.println(b1.company);
+		 
+		 BookVO b2 = new BookVO();
+		 System.out.println(b1.page);
+		 
+		 //생성자 메서드의 중복정의(overloading)
+		 BookVO b3 = new BookVO("python", 18000, "daerim", 920);
+		 System.out.println(b3.company);
+		 
+		 
+	}
+}
--- JavsTPC/src/kr/tpc/BookVO.java
+++ JavsTPC/src/kr/tpc/BookVO.java
@@ -7,4 +7,20 @@ public class BookVO {
 	public int page;
 	
 	// 디폴트생성자메서드(생략)
+	// 만약에 중복정의를하게되면 디폴트생성자는 저절로 만들어지지 않는다.
+	// 그래서 명시적으로 만들어줘야한다.
+	public BookVO() {
+		//초기화 작업
+		this.title="java";
+		this.price=14000;
+		this.company="easy";
+		this.page=700;
+	}
+	//생성자 메서드의 중복정의(overloading)
+	public BookVO(String title, int price, String company, int page) {
+		this.title=title;
+		this.price=price;
+		this.company=company;
+		this.page=page;
+	}
 }
